4bcc908a8209b793427f375b54c7b6a3d62eaa27
AbdelrahmanAbdelgawad <abdelrahman.abdelgawad@ejust.edu.eg>
2023-05-03 13:15:06 +0300

    fixed tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,4 +4,4 @@ from haptic.env.car_racer_env import HapticCarRacer
 def test_HapticCarRacer():
     env = HapticCarRacer()
     action_space = env.action_space.shape
-    assert action_space == (3,)
+    assert action_space[0] == 3
