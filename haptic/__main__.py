5d5bd8c70fbaca2132f16b60c328da882911278f
AbdelrahmanAbdelgawad <abdelrahman.abdelgawad@ejust.edu.eg>
2023-05-03 12:47:53 +0300

    dummy commit

--- haptic/__main__.py
+++ haptic/__main__.py
@@ -1,6 +1,15 @@
-def add(x, y):
-    return x + y
-
-
 if __name__ == "__main__":
-    add(2, 3)
+    import gym
+    from stable_baselines3 import PPO
+
+    env = gym.make("CarRacing-v0")
+
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=1000)
+    model.save("ppo_carracing")
+
+    # obs = env.reset()
+    # while True:
+    #     action, _states = model.predict(obs)
+    #     obs, rewards, dones, info = env.step(action)
+    #     env.render()
