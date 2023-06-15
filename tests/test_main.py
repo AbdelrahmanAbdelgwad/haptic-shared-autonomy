6e8757fce30d393d7d35dbfcfe8268b9539300a3
AbdelrahmanAbdelgawad <abdelrahman.abdelgawad@ejust.edu.eg>
2023-06-15 21:00:44 +0300

    trying to fix pytest error

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,20 +1,6 @@
 from haptic.__main__ import add
 from haptic.envs.car_racing import CarRacingDiscrete
 
-# from haptic.env.car_racer_env import subtract
-
-# from haptic.env.car_racer_env import HapticCarRacer
-
 
 def test_add():
     assert add(2, 3) == 5
-
-
-# def test_subtract():
-#     assert subtract(3, 2) == 1
-
-
-# def test_HapticCarRacer():
-#     env = HapticCarRacer()
-#     action_space = env.action_space.shape
-#     assert action_space[0] == 3
