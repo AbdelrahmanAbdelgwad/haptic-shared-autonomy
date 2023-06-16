a7e4a18a06bc776e38e7f2512f08c4d346f110b9
AbdelrahmanAbdelgawad <abdelrahman.abdelgawad@ejust.edu.eg>
2023-06-16 21:57:52 +0300

    added gym library as a module to the haptic package

---
+++ src/haptic/gym/envs/box2d/__init__.py
@@ -0,0 +1,4 @@
+from haptic.gym.envs.box2d.lunar_lander import LunarLander
+from haptic.gym.envs.box2d.lunar_lander import LunarLanderContinuous
+from haptic.gym.envs.box2d.bipedal_walker import BipedalWalker, BipedalWalkerHardcore
+from haptic.gym.envs.box2d.car_racing import CarRacing
---
+++ src/haptic/gym/envs/classic_control/__init__.py
@@ -0,0 +1,7 @@
+from haptic.gym.envs.classic_control.cartpole import CartPoleEnv
+from haptic.gym.envs.classic_control.mountain_car import MountainCarEnv
+from haptic.gym.envs.classic_control.continuous_mountain_car import (
+    Continuous_MountainCarEnv,
+)
+from haptic.gym.envs.classic_control.pendulum import PendulumEnv
+from haptic.gym.envs.classic_control.acrobot import AcrobotEnv
