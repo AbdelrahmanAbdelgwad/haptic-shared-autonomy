1144933e982186d3e09e0dd59122629072b16b7f
AbdelrahmanAbdelgawad <abdelrahman.abdelgawad@ejust.edu.eg>
2023-08-26 02:20:45 +0300

    added git submodule update --remote to .yml

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,5 @@
-from haptic.main import add
 from gym.envs.box2d.car_racing import CarRacing
 from gym.envs.box2d.car_racing import map_val
-
-
-def test_add():
-    assert add(2, 3) == 5
-
-
-def test_env():
-    env = CarRacing(
-        allow_reverse=False,
-        grayscale=1,
-        show_info_panel=1,
-        discretize_actions="hard",
-        num_tracks=2,
-        num_lanes=2,
-        num_lanes_changes=4,
-        max_time_out=0,
-        frames_per_state=4,
-    )
-    assert 1 == 1
 
 
 def test_map_val():
