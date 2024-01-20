c380031b45548f9d3936a711b527b8ddbe775b48
AbdelrahmanAbdelgawad <abdelrahman.abdelgawad@ejust.edu.eg>
2024-01-20 13:29:35 +0200

    added hyper-params

--- src/haptic/carla_gym/train.py
+++ src/haptic/carla_gym/train.py
@@ -57,7 +57,32 @@ if __name__ == "__main__":
         features_extractor_kwargs=dict(features_dim=1152),
     )
 
-    model = PPO(CustomActorCriticPolicy, env, verbose=1, policy_kwargs=policy_kwargs)
+    model = PPO(
+        CustomActorCriticPolicy,
+        env,
+        learning_rate=0.0003,
+        n_steps=2048,
+        batch_size=64,
+        n_epochs=10,
+        gamma=0.99,
+        gae_lambda=0.95,
+        clip_range=0.2,
+        clip_range_vf=None,
+        ent_coef=0.0,
+        vf_coef=0.5,
+        max_grad_norm=0.5,
+        use_sde=False,
+        sde_sample_freq=-1,
+        target_kl=None,
+        tensorboard_log=None,
+        create_eval_env=False,
+        policy_kwargs=None,
+        verbose=0,
+        seed=None,
+        device="auto",
+        _init_setup_model=True,
+        policy_kwargs=policy_kwargs,
+    )
 
     save_best_model_callback = SaveBestModelCallback(
         eval_env=env,
