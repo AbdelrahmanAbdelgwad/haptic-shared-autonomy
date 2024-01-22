2fcce92da4de945147ae2e68066b7ce7ad6ca7ad
AbdelrahmanAbdelgawad <abdelrahman.abdelgawad@ejust.edu.eg>
2024-01-22 17:06:07 +0200

    trained ppo-car-racing

--- src/haptic/architectures/custom_actor_critic.py
+++ src/haptic/architectures/custom_actor_critic.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import gym
 from gym.envs.box2d.car_racing import CarRacing
+from gym import wrappers
 import torch as th
 from torch import nn
 import torch.nn.functional as F
@@ -70,7 +71,14 @@ class CustomNetworkActorCritic(nn.Module):
         self.policy_net = nvidia_net.linear_layers
 
         # Value network
-        self.value_net = nvidia_net.linear_layers
+        self.value_net = nn.Sequential(
+            nn.Linear(in_features=1152, out_features=100),
+            nn.ELU(),
+            nn.Linear(in_features=100, out_features=50),
+            nn.ELU(),
+            nn.Linear(in_features=50, out_features=10),
+            nn.Linear(in_features=10, out_features=1),
+        )
 
     def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
         """
@@ -132,17 +140,17 @@ if __name__ == "__main__":
     FRAMES_PER_STATE = 4  # number of frames to stack for each state
     NVIDIA = True  # True for NVIDIA model, False for normal model with 96,96,3 input
     BUFFER_SIZE = 60_000
-    TOTAL_TIMESTEPS = 500_000
+    TOTAL_TIMESTEPS = 2000_000
 
     EVAL_FREQ = 100_000  # number of timesteps between each evaluation
     RENDER_EVAL = False  # True if you want to render the evaluation
     OUTPUT_PATH = "./training_folder"  # path to save the training folder
     MODEL_SAVE_FREQ = 100_000  # number of timesteps between each model save
 
-    today = date.today()
-    date_str = today.strftime("%b-%d-%Y")
-    train_folder_output_path = f"{OUTPUT_PATH}/{date_str}_{time()}"
-    os.makedirs(train_folder_output_path)
+    TRAIN = False
+
+    NO_EPISODES = 1000
+    BETA = 1
 
     env = CarRacing(
         allow_reverse=False,
@@ -157,32 +165,80 @@ if __name__ == "__main__":
         nvidia=NVIDIA,
     )
 
-    policy_kwargs = dict(
-        features_extractor_class=CNNFeatureExtractor,
-        features_extractor_kwargs=dict(features_dim=1152),
-    )
+    if TRAIN:
+        today = date.today()
+        date_str = today.strftime("%b-%d-%Y")
+        train_folder_output_path = f"{OUTPUT_PATH}/{date_str}_{time()}"
+        os.makedirs(train_folder_output_path)
 
-    model = PPO(CustomActorCriticPolicy, env, verbose=1, policy_kwargs=policy_kwargs)
+        policy_kwargs = dict(
+            features_extractor_class=CNNFeatureExtractor,
+            features_extractor_kwargs=dict(features_dim=1152),
+        )
 
-    save_best_model_callback = SaveBestModelCallback(
-        eval_env=env,
-        n_eval_episodes=1,
-        logpath=f"{train_folder_output_path}/logs",
-        savepath=f"{train_folder_output_path}/best_model",
-        eval_frequency=EVAL_FREQ,
-        verbose=1,
-        render=RENDER_EVAL,
-    )
-    save_model_callback = PeriodicSaveModelCallback(
-        save_frequency=MODEL_SAVE_FREQ,
-        save_path=f"{train_folder_output_path}/models",
-    )
-    callbacks = CallbackList([save_best_model_callback, save_model_callback])
+        model = PPO(
+            CustomActorCriticPolicy,
+            env,
+            verbose=1,
+            policy_kwargs=policy_kwargs,
+            learning_rate=0.0001,
+            n_steps=2048,
+            batch_size=64,
+            n_epochs=10,
+            gamma=0.99,
+            gae_lambda=0.95,
+            clip_range=0.2,
+            clip_range_vf=None,
+        )
 
-    model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callbacks)
+        save_best_model_callback = SaveBestModelCallback(
+            eval_env=env,
+            n_eval_episodes=1,
+            logpath=f"{train_folder_output_path}/logs",
+            savepath=f"{train_folder_output_path}/best_model",
+            eval_frequency=EVAL_FREQ,
+            verbose=1,
+            render=RENDER_EVAL,
+        )
+        save_model_callback = PeriodicSaveModelCallback(
+            save_frequency=MODEL_SAVE_FREQ,
+            save_path=f"{train_folder_output_path}/models",
+        )
+        callbacks = CallbackList([save_best_model_callback, save_model_callback])
 
-    model.save("actor_critic_car_racing")
+        model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callbacks)
 
+        model.save("actor_critic_car_racing")
+
+    else:
+        env = wrappers.Monitor(
+            env,
+            f"./videos_ppo/",
+            force=True,
+        )
+        model = PPO.load("training_folder/Jan-21-2024_1705789568.5404365/best_model")
+        episode_timesteps = 0
+        done = False
+        episode_reward = 0
+        total_timesteps = 0
+        total_reward = 0
+        observation = env.reset()
+        action_prev = 0
+        for episode in range(NO_EPISODES):
+            while not done:
+                episode_timesteps += 1
+                total_timesteps += 1
+                env.render()
+                action, _ = model.predict(observation)
+                action = BETA * action + (1 - BETA) * action_prev
+                action_prev = action
+                observation, reward, done, info = env.step(action)
+                episode_reward += reward
+                total_reward += reward
+                if done:
+                    env.reset()
+                    done = False
+        env.close()
     # action_noise = OrnsteinUhlenbeckActionNoise(
     #     mean=np.array([0]),
     #     sigma=np.array([0.4]),
